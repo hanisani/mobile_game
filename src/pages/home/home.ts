16ce7546f04bb80b525e9608836801fb26c61d4e
hanisani <faheemahmad@gmail.com>
2017-10-26 13:59:47 +0800

    Commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,39 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  tapped = 0;
+  pressed = 0;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onDidReset(resetType: string) {
+    switch (resetType) {
+      case 'tap':
+        this.tapped = 0;
+        break;
+      case 'press':
+        this.pressed = 0;
+        break;
+      default:
+        this.tapped = 0;
+        this.pressed = 0;
+    }
+  }
+
+  onTap() {
+    console.log('Tapped');
+    this.tapped++;
+  }
+
+  onPress() {
+    console.log('Pressed');
+    this.pressed++;
+  }
+
+  didWin() {
+    return this.tapped == 2 && this.pressed == 4;
   }
 
 }
